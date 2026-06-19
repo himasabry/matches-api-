4785eca4a779a758d4826dee058bde0168bf9333
himasabry <36902869+himasabry@users.noreply.github.com>
2026-06-19 22:23:06 +0200

    Update matches.js

--- api/matches.js
+++ api/matches.js
@@ -1,23 +1,18 @@
+import { matches } from "../data/matches.js";
+
 export default function handler(req, res) {
-
-  const matchesData = [
-    {
-      league: "مباراة ودية",
-      time: "2026-03-27T20:00:00",
-      team1: { name: "ليبيا" },
-      team2: { name: "النيجر" },
-      channels: [{ name: "ليبيا الرياضية", url: "go:lybiasp" }]
-    }
-  ];
-
   const { league, team } = req.query;
 
-  let data = matchesData;
+  let data = matches;
 
+  // فلترة بالـ league
   if (league) {
-    data = data.filter(m => m.league.includes(league));
+    data = data.filter(m =>
+      m.league.includes(league)
+    );
   }
 
+  // فلترة بالـ team
   if (team) {
     data = data.filter(m =>
       m.team1.name.includes(team) ||
@@ -28,6 +23,6 @@ export default function handler(req, res) {
   res.status(200).json({
     success: true,
     count: data.length,
-    data
+    matches: data
   });
 }
