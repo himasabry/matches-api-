f069441bd95adc6881c20fa406c7935182581385
himasabry <36902869+himasabry@users.noreply.github.com>
2026-06-19 22:30:35 +0200

    Update matches.js

--- api/matches.js
+++ api/matches.js
@@ -1,28 +1,9 @@
 import { matches } from "../data/matches.js";
 
 export default function handler(req, res) {
-  const { league, team } = req.query;
-
-  let data = matches;
-
-  // فلترة بالـ league
-  if (league) {
-    data = data.filter(m =>
-      m.league.includes(league)
-    );
-  }
-
-  // فلترة بالـ team
-  if (team) {
-    data = data.filter(m =>
-      m.team1.name.includes(team) ||
-      m.team2.name.includes(team)
-    );
-  }
-
   res.status(200).json({
     success: true,
-    count: data.length,
-    matches: data
+    count: matches.length,
+    matches
   });
 }
